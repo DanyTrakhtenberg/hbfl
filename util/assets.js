ce4355cd5a01270c2113e4f6bd96feacf03b8bfb
danytrakhtenberg <danytrakhtenberg@gmail.com>
2021-03-07 00:47:35 +0200

    another change in cloud from url

--- util/assets.js
+++ util/assets.js
@@ -1,4 +1,4 @@
-const base = 'https://d2fj0qy79x6dup.cloudfront.net'
+const base = 'https://d3u8d4ac19oy59.cloudfront.net'
 module.exports = {
   hamster1: base + '/images/hamster-1-final.png',
   hamster2: base + '/images/hamster-2-final.png',
